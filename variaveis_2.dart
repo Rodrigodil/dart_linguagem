3c32d1f9f36c8d0b6633eea6d021843714badf08
Rodrigo <rodrigodil@live.com>
2025-01-22 17:57:38 -0300

    Inicio

---
+++ variaveis_2.dart
@@ -0,0 +1,15 @@
+main() {
+  
+  var r1 = 2;
+  var r2 = 4.56;
+  var r3 = "Olá";
+
+  print(r1 + r2);
+
+  print(r1.runtimeType);
+  print(r2.runtimeType);
+  print(r3.runtimeType);
+
+  print(r1 is int);
+  print(r1 is double);
+}
